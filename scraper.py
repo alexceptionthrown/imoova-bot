dc70bebf75c60465cc504973fe27a14950dc8379
Alexandre Riou <224705868+alexceptionthrown@users.noreply.github.com>
2025-08-10 10:02:07 -0400

    update scraper to fit visualization

--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup, Tag
 
-BASE_URL = "https://www.imoova.com/en/relocations?region=US"
+BASE_URL = "https://www.imoova.com/"
+USA_URI = "/en/relocations?region=US"
 
 
 def parse_offer(offer: Tag) -> dict[str, str]:
@@ -13,7 +14,7 @@ def parse_offer(offer: Tag) -> dict[str, str]:
             class_='mb-3 flex flex-wrap items-center gap-3 border-b border-gray-100 pb-3 sm:mb-4 sm:gap-4 sm:pb-4'
         ).find_all('div')
     ]
-    length = offer.find(
+    duration = offer.find(
         'span',
         class_="inline-flex items-center rounded-full border "
                "border-blue-200 bg-blue-50 px-2.5 py-1 text-xs font-medium text-blue-700 sm:px-3 sm:py-1.5"
@@ -26,17 +27,17 @@ def parse_offer(offer: Tag) -> dict[str, str]:
         'origin': origin_destination[0],
         'destination': origin_destination[1],
         'dates': offer.find('time').text,
-        'beds': attributes[2],
-        'length': length
+        'seats': attributes[0],
+        'duration': duration
     }
     return offer_dict
 
 
 if __name__ == "__main__":
-    page = requests.get(BASE_URL)
+    page = requests.get(BASE_URL + USA_URI)
     soup = BeautifulSoup(page.content, 'html.parser')
     offer_class_type = ("focus:outline-hidden flex flex-col rounded-2xl focus:ring-2 "
                         "focus:ring-amber-400/50 focus:ring-offset-2 md:flex-row")
     offers = soup.find_all("a", class_=offer_class_type)
     for offer in offers:
-        print(offer.text)
+        print(parse_offer(offer))
